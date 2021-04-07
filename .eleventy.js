71033e22f6c69432e0910d55ce0ff998b8980cde
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2021-04-07 13:15:44 -0500

    Config deploy settings

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 const sortByDisplayOrder = require("./src/utils/sort-by-display-order.js");
 
+// Transforms
+const htmlMinTransform = require("./src/transforms/html-min-transform.js");
+
+// Create a helpful production flag
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = (config) => {
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy("./src/images/");
@@ -19,6 +25,11 @@ module.exports = (config) => {
             collection.getFilteredByGlob("./src/products/*.md")
         ).filter((x) => x.data.featured);
     });
+
+    // Only minify HTML if we are in production because it slows builds _right_ down
+    if (isProduction) {
+        config.addTransform("htmlmin", htmlMinTransform);
+    }
 
     return {
         markdownTemplateEngine: "njk",
